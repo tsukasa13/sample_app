13199d207895dc8482ed695e922b9905cde80086
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2024-01-24 12:22:54 +0000

    [Add] 3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,13 +5,9 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(list_params)
-    if @list.save
-      flash[:notice] = "投稿が成功しました"
-      redirect_to list_path(@list.id)
-    else
-      render :new
-    end
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
   end
 
   def index
